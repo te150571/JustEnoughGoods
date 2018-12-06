119fceb865b8a94bc1802669c9dcf7be32a67e75
te4a140170 <te140170@gmail.com>
2018-12-06 12:57:11 +0900

    commit22

--- app/src/main/java/com/jeg/te/justenoughgoods/MainActivity.java
+++ app/src/main/java/com/jeg/te/justenoughgoods/MainActivity.java
@@ -1,13 +1,19 @@
 package com.jeg.te.justenoughgoods;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.syokai);
+        setContentView(R.layout.menu);
     }
+
+    
 }
